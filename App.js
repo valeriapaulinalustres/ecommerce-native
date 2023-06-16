512c89928e92f44f43949c721f27312f8455fd68
valeriapaulinalustres <valeriapaulinalustres@yahoo.com.ar>
2023-06-16 15:59:45 -0300

    hola

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My ecommerce en React Native</Text>
+      <Text>My ecommerce en React Native. Hola Coder!</Text>
       <StatusBar style="auto" />
     </View>
   );
